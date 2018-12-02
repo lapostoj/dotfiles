2a428bc9c7895197fb6eeadd9e914c6090a5201b
Jerome Lapostolet <jerome.lapostolet@marshmallow.co>
2018-12-02 18:12:36 +0000

    Fix brew-upgrade-process

--- homebrew/brew.zsh
+++ homebrew/brew.zsh
@@ -2,4 +2,4 @@ export BREW_CHANGELOG_FILE=$DOTFILES/homebrew/brew_changelog
 export HOMEBREW_NO_AUTO_UPDATE=1
 
 # Customized commands
-alias brew-upgrade-process='$DOTFILES/homebrew/upgrade.sh'
+alias brew-upgrade-process='$DOTFILES/homebrew/brew_upgrade_process.sh'
