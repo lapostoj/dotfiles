401479fb730a0880c31dbb410bb3f8f35dfd9c39
Jerome Lapostolet <jerome.lapostolet@gmail.com>
2022-10-08 12:28:24 +0100

    Update homebrew install command

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,7 +4,7 @@ if test ! $(which brew)
 then
   echo "🍺 Installing Homebrew for you."
 
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else 
   echo "🍻 Homebrew already installed."
 fi
